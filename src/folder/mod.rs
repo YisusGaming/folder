d6a40e49e8793a3f4baa6c6cf13787819d8f6d08
Yisus <jesusgameroom@gmail.com>
2023-03-04 15:15:50 -0400

    output formatter

--- src/folder/mod.rs
+++ src/folder/mod.rs
@@ -1,5 +1,5 @@
-use std::process::Command;
-use std::io::{self, Write};
+use std::process::{self, Command};
+use std::str::from_utf8;
 
 #[derive(PartialEq)]
 pub enum Mode {
@@ -11,6 +11,15 @@ pub enum Mode {
 pub struct FolderConfig {
     pub mode: Mode,
     pub dir_name: String
+}
+
+/// Takes a buffer and returns an String
+/// replacing "mkdir: " for "folder: ".
+pub fn format_output(buf: &Vec<u8>) -> String {
+    let str = from_utf8(&buf).unwrap_or("");
+    let str = str.replace("mkdir: ", "folder: ");
+    
+    String::from(str.trim())
 }
 
 pub fn resolve_mode(arg: &String) -> Mode {
@@ -29,9 +38,17 @@ pub fn run(config: &FolderConfig) {
             .arg("-c")
             .arg(format!("mkdir -v {}", config.dir_name))
             .output()
-            .expect("Failed to created folder");
+            .expect("Failed to create folder");
     
-        io::stdout().write_all(&command.stdout).unwrap();
-        io::stderr().write_all(&command.stderr).unwrap();
+        let stdout = format_output(&command.stdout);
+        let stderr = format_output(&command.stderr);
+
+        if stdout != "" {
+            println!("{}", stdout);
+        }
+        if stderr != "" {
+            eprintln!("{}", stderr);
+            process::exit(1);
+        }
     }
 }
