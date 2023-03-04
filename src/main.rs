b6ef9e9cb8fe04e7594e6591753318ae7e09ff81
Yisus <jesusgameroom@gmail.com>
2023-03-04 13:54:14 -0400

    project setup + cli argument parser

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::process;
+use std::env;
+use folder::{FolderConfig, Mode};
+
+pub mod folder;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(args);
+    folder::run(&config);
+}
+
+pub fn parse_config(args: Vec<String>) -> FolderConfig {
+    if args.len() < 3 {
+        println!("folder: More arguments needed");
+        process::exit(-1);
+    }
+
+    let mode = folder::resolve_mode(&args[1]);
+    match mode {
+        Mode::UNKNOW => {
+            println!("folder: Unknow mode {}", &args[1]);
+            process::exit(-1);
+        }
+        _ => ()
+    };
+
+    let dir_name = String::from(&args[2]);
+
+    FolderConfig { mode, dir_name }
+}
