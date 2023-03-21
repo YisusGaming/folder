0a421620c328b9af4dc18f521f1c9787ccef38ce
Yisus <jesusgameroom@gmail.com>
2023-03-20 23:34:13 -0400

    cli options added

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+
 // Should be the same as the one specified
 // at the Cargo.toml file.
 pub const VERSION: &str = "1.0.0";
+
+
+#[derive(PartialEq)]
+pub enum Options {
+    HELP,
+    VERSION
+}
+fn options_map() -> HashMap<String, Options> {
+    let mut map: HashMap<String, Options> = HashMap::new();
+
+    map.insert(String::from("--help"), Options::HELP);
+    map.insert(String::from("--version"), Options::VERSION);
+
+    map
+}
