299d269d493b58cf7f29df2943830000415f7ada
Yisus <jesusgameroom@gmail.com>
2023-07-17 23:19:16 -0400

    fixed and finished option parser

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -20,13 +20,23 @@ fn options_map() -> HashMap<String, Options> {
     map
 }
 
+/// Takes in all the cli arguments as a parameter and
+/// resolves which arguments are valid cli options.
+/// 
+/// Automatically executes the options found.
 pub fn parse_options(args: &Vec<String>) {
     let map = options_map();
     let mut options: Vec<&Options> = Vec::new();
 
     if args.len() == 0 { return };
 
-    for arg in args { // FIXME: This is always returning Options::UNKNOW
+    for (i, arg) in args.iter().enumerate() {
+        // We skip the first argument since
+        // it's traditionally the path of the executable.
+        if i == 0 { continue; }
+        // We also ignore any argument not formatted as an option.
+        if !arg.starts_with("--") { continue; }
+
         let option = map.get(arg).unwrap_or(&Options::UNKNOW);
         options.push(option);
     }
@@ -39,10 +49,18 @@ pub fn run_options(ops: Vec<&Options>) {
     for op in ops {
         if op == &Options::HELP {
             println!("Help message");
+
+            // Options like this would trigger a premature exit.
+            process::exit(0);
         } else if op == &Options::VERSION {
-            println!("Folder version {VERSION}.");
+            println!("Folder v{VERSION}");
+            println!("Made by Yisus. 2023.\n@ItsJustYisus & @yisuscoding on Youtube.");
+            println!("Happy Hacking!");
+
+            // Options like this would trigger a premature exit.
+            process::exit(0);
         } else if op == &Options::UNKNOW {
-            eprintln!("folder: An unknow options was passed.");
+            eprintln!("folder: An unknow option was passed.");
             process::exit(1);
         }
     }
